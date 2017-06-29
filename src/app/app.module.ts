7e712965725a852d75499c50b5791812705a88b5
Kumaresan <kumaresh876@gmail.com>
2017-06-29 13:57:25 +0530

    Navbar Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FacebookModule } from 'ngx-facebook';
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    NavbarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FacebookModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/app.services.ts
@@ -0,0 +1,14 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+
+export class AppServices{
+    
+    private username:string;
+    private client_id = '';
+    private  client_secret = '';
+}
+
